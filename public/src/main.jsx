9430c588a7ed09ba70d98b724956c02f461a1255
dhruvang-uplers <72018139+dhruvang-uplers@users.noreply.github.com>
2022-01-21 19:05:10 +0530

    Rollback Redux-persist

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import store from './Redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+// import { PersistGate } from 'redux-persist/integration/react'
+// import { persistStore } from 'redux-persist'
 import './index.css'
 import App from './App'
 
